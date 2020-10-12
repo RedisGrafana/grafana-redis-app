f2569d11df0d2de8be20dd732dd3e4ee16753c4b
Mikhail <mikhail@redislabs.com>
2020-10-11 20:17:31 -0700

    Update RootPage

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,4 @@
-import { RootPage } from 'RootPage';
+import { RootPage } from 'components';
 import { AppPlugin } from '@grafana/data';
 import { ConfigCtrl } from './legacy/config';
 import { GlobalSettings } from './types';
