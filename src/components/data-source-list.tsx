9f45f1000621fbaaac4c76dedd16a50dc58ba1e3
Mikhail <47795110+mikhailredis@users.noreply.github.com>
2020-10-12 23:12:37 -0700

    Update Root page with data sources (#6)
    * Rename configuration as legacy
    
    * Add Application screenshot
    
    * Add Icons
    
    * Update Root Page

---
+++ src/components/data-source-list.tsx
@@ -0,0 +1,140 @@
+import {
+  HighAvailability,
+  MultiLayerSecurity,
+  RedisAI,
+  RedisBloom,
+  RedisCube,
+  RedisGears,
+  RedisGraph,
+  RedisJSON,
+  RedisTimeSeries,
+} from 'icons';
+import React, { FC } from 'react';
+import { Container, HorizontalGroup, InfoBox, LinkButton, VerticalGroup } from '@grafana/ui';
+
+/**
+ * Properties
+ */
+interface Props {
+  /**
+   * Data sources
+   *
+   * @type {any[]}
+   */
+  datasources?: any[];
+}
+
+export const DataSourceList: FC<Props> = ({ datasources }) => {
+  /**
+   * Check if any data sources was added
+   */
+  if (datasources?.length === 0) {
+    return (
+      <div>
+        <div className="page-action-bar">
+          <div className="page-action-bar__spacer" />
+          <LinkButton href="datasources/new" icon="database">
+            Add data source
+          </LinkButton>
+        </div>
+        <InfoBox title="Please add Redis Data Sources." url={'https://grafana.com/plugins/redis-datasource'}>
+          <p>You can add as many data sources as you want to support multiple Redis databases.</p>
+        </InfoBox>
+      </div>
+    );
+  }
+
+  /**
+   * Return
+   */
+  return (
+    <div>
+      <div className="page-action-bar">
+        <div className="page-action-bar__spacer" />
+        <LinkButton href="datasources/new" icon="database">
+          Add data source
+        </LinkButton>
+      </div>
+
+      <section className="card-section card-list-layout-list">
+        <ol className="card-list">
+          {datasources?.map((redis, index) => {
+            const title = redis.commands?.length ? 'Working as expected' : "Can't retrieve a list of commands";
+            const fill = redis.commands?.length ? '#DC382D' : '#A7A7A7';
+
+            return (
+              <li className="card-item-wrapper" key={index} aria-label="check-card">
+                <a className="card-item" href={`/datasources/edit/${redis.id}`}>
+                  <HorizontalGroup justify="space-between">
+                    <HorizontalGroup justify="flex-start">
+                      <Container margin="xs">
+                        <RedisCube size={32} fill={fill} title={title} />
+                      </Container>
+                      <VerticalGroup>
+                        <div className="card-item-name">{redis.name}</div>
+                        <div className="card-item-sub-name">{redis.url}</div>
+                      </VerticalGroup>
+                    </HorizontalGroup>
+
+                    <HorizontalGroup justify="flex-end">
+                      {!redis.commands?.length && (
+                        <div className="card-item-header">
+                          <div className="card-item-type">{title}</div>
+                        </div>
+                      )}
+                      {(redis.jsonData['tlsAuth'] || redis.jsonData['acl']) && (
+                        <Container margin="xs">
+                          <MultiLayerSecurity size={32} fill={fill} />
+                        </Container>
+                      )}
+                      {redis.jsonData['client']?.match(/cluster|sentinel/) && (
+                        <Container margin="xs">
+                          <HighAvailability size={32} fill={fill} />
+                        </Container>
+                      )}
+                      {redis.commands?.indexOf('RG.PYEXECUTE') !== -1 && (
+                        <Container margin="xs">
+                          <RedisGears size={32} fill={fill} />
+                        </Container>
+                      )}
+                      {redis.commands?.indexOf('TS.INFO') !== -1 && (
+                        <Container margin="xs">
+                          <RedisTimeSeries size={32} fill={fill} />
+                        </Container>
+                      )}
+                      {redis.commands?.indexOf('AI.INFO') !== -1 && (
+                        <Container margin="xs">
+                          <RedisAI size={32} fill={fill} />
+                        </Container>
+                      )}
+                      {redis.commands?.indexOf('FT.INFO') !== -1 && (
+                        <Container margin="xs">
+                          <RedisGears size={32} fill={fill} />
+                        </Container>
+                      )}
+                      {redis.commands?.indexOf('JSON.GET') !== -1 && (
+                        <Container margin="xs">
+                          <RedisJSON size={32} fill={fill} />
+                        </Container>
+                      )}
+                      {redis.commands?.indexOf('GRAPH.QUERY') !== -1 && (
+                        <Container margin="xs">
+                          <RedisGraph size={32} fill={fill} />
+                        </Container>
+                      )}
+                      {redis.commands?.indexOf('BF.INFO') !== -1 && (
+                        <Container margin="xs">
+                          <RedisBloom size={32} fill={fill} />
+                        </Container>
+                      )}
+                    </HorizontalGroup>
+                  </HorizontalGroup>
+                </a>
+              </li>
+            );
+          })}
+        </ol>
+      </section>
+    </div>
+  );
+};
