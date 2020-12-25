6822b8d036615f5b254647025b0e342f01d1fd0a
Alex Simonok <sashasimonok@gmail.com>
2020-12-25 08:46:40 +0300

    Add tests for Redis CLI panel (#24)

--- src/redis-cli-panel/module.test.ts
+++ src/redis-cli-panel/module.test.ts
@@ -1,8 +1,19 @@
-/**
- * Just a stub test
+import { PanelPlugin } from '@grafana/data';
+import { plugin } from './module';
+
+/*
+ Plugin
  */
-describe('placeholder test', () => {
-  it('should return true', () => {
-    expect(true).toBeTruthy();
+describe('plugin', () => {
+  it('should be instance of PanelPlugin', () => {
+    expect(plugin).toBeInstanceOf(PanelPlugin);
+  });
+
+  it('Should add textarea height input', () => {
+    const builder = {
+      addNumberInput: jest.fn(),
+    };
+    plugin['registerOptionEditors'](builder);
+    expect(builder.addNumberInput).toHaveBeenCalledWith({ path: 'height', name: 'Textarea height', defaultValue: 12 });
   });
 });
