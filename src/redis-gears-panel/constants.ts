717684c4462742f7ed60c793fc9c4f4f30ad5c6d
Mikhail <47795110+mikhailredis@users.noreply.github.com>
2021-04-19 20:12:01 -0400

    Update RedisGears Execution Modes

--- src/redis-gears-panel/constants.ts
+++ src/redis-gears-panel/constants.ts
@@ -2,3 +2,25 @@
  * Default script
  */
 export const DefaultScript = 'gb = GearsBuilder()';
+
+/**
+ * Execution Mode
+ */
+export enum ExecutionMode {
+  Blocking = 0,
+  Unblocking = 1,
+}
+
+/**
+ * Unblocking options
+ */
+export const ExecutionOptions = [
+  {
+    label: 'Blocking',
+    value: ExecutionMode.Blocking,
+  },
+  {
+    label: 'Unblocking',
+    value: ExecutionMode.Unblocking,
+  },
+];
